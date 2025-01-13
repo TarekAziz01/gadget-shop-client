0c3bdfea211487f8d7bf73cc9f7496f3e6ab9f26
Tarek Aziz <mdaziz3289@gmail.com>
2025-01-13 11:41:45 +0600

    siderbar

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,17 +1,30 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import { GrOverview } from "react-icons/gr";
+import { IoHomeOutline } from "react-icons/io5";
+import { BiLogOut } from "react-icons/bi";
 
 const Sidebar = () => {
   return (
-    <div className="bg-gray-200 border-r-2 border-black min-h-screen px-2">
+    <div className="bg-gray-200 border-r-2 border-black min-h-screen lg:px-8 lg:py-16 px-6 py-10">
+      <h1 className="text-3xl font-bold mb-8">Gadget Shop</h1>
       <ul className="flex flex-col gap-2">
-        <li>
-          <NavLink to="/dashboard/overview">OverView</NavLink>
+        <li className="p-2 border border-black rounded-md">
+          <NavLink to="/dashboard/overview" className="flex items-center gap-2">
+            <GrOverview />
+            <p>OverView</p>
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="/">Home</NavLink>
+        <li className="p-2 border border-black rounded-md">
+          <NavLink to="/dashboard/overview" className="flex items-center gap-2">
+            <IoHomeOutline />
+            <p>Home</p>
+          </NavLink>
         </li>
-        <li>
-          <button>Logout</button>
+        <li className="p-2 border border-black rounded-md">
+          <NavLink to="/dashboard/overview" className="flex items-center gap-2">
+            <BiLogOut />
+            <p>LogOut</p>
+          </NavLink>
         </li>
       </ul>
     </div>
